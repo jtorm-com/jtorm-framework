aafef113169d0dee6181cce5993741b9f892f96f
rinnert <rinnert.deelstra@gmail.com>
2022-01-03 11:18:44 +0100

    Refactoring and optimize code

--- src/handler/src/handler.js
+++ src/handler/src/handler.js
@@ -1,5 +1,6 @@
 /*! (c) jTorm and other contributors | www.jtorm.com/license */
 'use strict';
+
 module.exports = {
     jTormHandler: {
         context: {},
@@ -14,12 +15,18 @@ module.exports = {
             return v.h;
         },
         handleChildren: async function (h, t, m, v) {
-            var s = this, k, t2 = s.context._.cloneDeep(t), v2;
+            let s = this,
+                sC = s.context,
+                cM = sC.models,
+                e = cM.event,
+                k,
+                t2 = sC._.cloneDeep(t),
+                v2;
 
             for (k in t2.c)
                 t2.c[k].s = 'body';
 
-            v2 = await s.context.models.view.create(s, "<body>" + h + "</body>", t2.c, m, v.c);
+            v2 = await cM.view.create(s, "<body>" + h + "</body>", t2.c, m, v.c);
             v2.cid = v.cid;
 
             if (t.s)
@@ -30,24 +37,32 @@ module.exports = {
                 c: 1
             };
 
-            await s.context.models.event.handle(s, v2, 'before', 'iteration');
+            await e.handle(s, v2, 'before', 'iteration');
 
             if (!v.r)
                 v.r = await s.handle("<body>" + h + "</body>", t2.c, m, v.c, v2);
 
             v2.cid = v.cid;
 
-            await s.context.models.event.handle(s, v2, 'after', 'iteration');
+            await e.handle(s, v2, 'after', 'iteration');
 
             return v.r;
         },
         parse: async function (v) {
-            let s = this, m, ms = s.context.methods, k, k2;
+            let s = this,
+                sC = s.context,
+                e = sC.models.event,
+                m,
+                ms = sC.methods,
+                k,
+                k2;
+
             for (k in v.tss) {
                 v.t = v.tss[k];
 
                 do {
                     m = 0;
+
                     if (v.t.m && ms[v.t.m])
                         m = ms[v.t.m];
                     else {
@@ -63,25 +78,26 @@ module.exports = {
                         if (v._.isFunction(m.data))
                             await m.data(s, v);
                         else
-                            s.context.parsers.data.handle(s, v, m.params);
+                            sC.parsers.data.handle(s, v, m.params);
 
                         v.io.v = await m.validate(s, v);
 
-                        await s.context.models.event.handle(s, v, 'before', 'method');
+                        await e.handle(s, v, 'before', 'method');
+
                         if (v.io.v && v._.isFunction(m.handle))
                             await m.handle(s, v);
                         else
                             v.io.r = 0;
-                        await s.context.models.event.handle(s, v, 'after', 'method');
+
+                        await e.handle(s, v, 'after', 'method');
                     } else {
                         v.io.r = 0;
                         v.io.c = 1;
                     }
                 } while (v.io.r);
 
-                if (v.io.c && v.t.c.length) {
+                if (v.io.c && v.t.c.length)
                     await s.handle(v.h, v.t.c, v.io.d ? v.io.d : v.m, v.c);
-                }
 
                 v.io.d = null;
             }
