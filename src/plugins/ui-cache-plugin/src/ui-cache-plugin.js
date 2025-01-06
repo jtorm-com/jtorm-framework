2abc2e2efcd2d7316ccee58335aad0faf4e446c0
Rinnert Deelstra <rinnert.deelstra@gmail.com>
2025-01-06 14:21:53 +0100

    Refactor

--- src/plugins/ui-cache-plugin/src/ui-cache-plugin.js
+++ src/plugins/ui-cache-plugin/src/ui-cache-plugin.js
@@ -24,12 +24,14 @@ module.exports = {
 
         beforeIteration: async function (v) {
             if (v.cid)
-                v.r = await this.uiCacheModel.get(v, v.l, v.cid, v.cs ? v.cs : 'default');
+                v.r = await this.uiCacheModel.get(v, v.l, v.cid, v.cs ? v.cs : 'default')
+            ;
         },
 
         afterIteration: function (v) {
             if (v.cid)
-                this.uiCacheModel.set(v, v.l, v.cid, v.cs ? v.cs : 'default', v.h.body());
+                this.uiCacheModel.set(v, v.l, v.cid, v.cs ? v.cs : 'default', v.r.body())
+            ;
         },
 
         afterView: function (v) {
